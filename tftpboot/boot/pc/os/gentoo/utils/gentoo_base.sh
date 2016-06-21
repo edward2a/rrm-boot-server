2620781acbf94c420cf20252f4ba61133fce38f9
niteman <eduardoaparis@gmail.com>
2016-06-21 06:52:11 +0100

    replace with some symlinks for avoiding duplicate stuff

--- tftpboot/boot/pc/os/gentoo/utils/gentoo_base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-base_url='http://distfiles.gentoo.org/releases/amd64/autobuilds'
-
-get_current_stage3() {
-    url="${base_url}/latest-stage3-amd64.txt"
-    data=$(wget -qO- ${url})
-}
