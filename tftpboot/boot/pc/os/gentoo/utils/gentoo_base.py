cdcb1c6349e7984b661ae28cb9376207a5967b32
niteman <eduardoaparis@gmail.com>
2016-06-21 06:45:53 +0100

    Initial booting version

---
+++ tftpboot/boot/pc/os/gentoo/utils/gentoo_base.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import sys
+
+
+class GentooStage3(object):
+
+    base_url = 'http://distfiles.gentoo.org/releases/amd64/autobuilds/'
+    stage3_latest = 'latest-stage3-amd64.txt'
+    stage3_dl = None
+
+    def __init__(self):
+        self.stage3_dl = self.get_latest_stage3_link()
+        self.get_latest_stage3_archive()
+
+        return None
+
+    def get_latest_stage3_link(self):
+        _url = self.base_url + self.stage3_latest
+        try:
+            data = urllib.request.urlopen(url=_url, timeout=5).read()
+        except:
+            raise
+
+        result = data.splitlines()[2].decode('utf-8').split()
+        sys.stdout.writelines(
+                'Found ' + result[0] + 'with size (bytes): ' + result[1] + '\n'
+            )
+        return result
+
+    def get_latest_sum(self):
+        _url = self.base_url + self.stage3_dl[0].DIGESTS
+        try:
+            data = urllib.request.urlopen(url=_url, timeout=5).read()
+        except:
+            raise
+
+        self.stage3_sum = data.splitlines()[1].split()[0].decode('utf-8')
+
+    def get_latest_stage3_archive(self):
+        _url = self.base_url + self.stage3_dl[0]
+        dest_file = self.stage3_dl[0].split('/')[1]
+        try:
+            sys.stdout.writelines('Downloading ' + dest_file + '...\n')
+            data = urllib.request.urlretrieve(url=_url, filename=dest_file)
+            sys.stdout.writelines('Download finished.\n')
+        except:
+            raise
+
+        return None
+
+if __name__ == '__main__':
+    try:
+        GentooStage3()
+    except:
+        sys.exit(1)
