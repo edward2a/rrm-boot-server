bfb0704480487bf19e0e696766164e069148e9ad
Eduardo Paris Penas <edward2a@gmail.com>
2016-06-26 18:01:41 +0100

    add sum calculation for local copy of stage3

--- tftpboot/os/gentoo/utils/gentoo_base.py
+++ tftpboot/os/gentoo/utils/gentoo_base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import hashlib
 import urllib.request
 import sys
 
@@ -37,6 +38,17 @@ class GentooStage3(object):
             raise
 
         self.stage3_sum = data.splitlines()[1].split()[0].decode('utf-8')
+
+    def calculate_file_sum(self, target_file):
+        buffer_size = 65536
+        try:
+            hash = hashlib.sha512()
+            with open(target_file, 'rb') as f:
+                for chunk in iter(lambda: f.read(buffer_size), b''):
+                    hash.update(chunk)
+            return hash.hexdigest()
+        except FileNotFoundError:
+            return None
 
     def get_latest_stage3_archive(self):
         _url = self.base_url + self.stage3_dl[0]
